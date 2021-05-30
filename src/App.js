c0ee3a134a5e6ad7d0b2b31220c09a187f260181
Farhan Oktavian <farhanoktvn@gmail.com>
2021-05-30 15:33:53 +0100

    added simple objects

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
+// import Box from './Box';
+import Cylinder from './Cylinder'
 
 function App() {
-  const [active, setActive] = useState(false)
   return (
     <div id="canvas-container">
-      <Canvas>
-        <ambientLight
-          intensity={0.1}
-        />
-
-        <directionalLight
-          color="white"
-          position={[0, 0, 5]}
-        />
-
-        <mesh
-          scale={active ? 1.5 : 1}
-          onClick={() => setActive(!active)}
-        >
-          <boxGeometry
-            args={[3, 3, 3]}
-          />
-          <meshNormalMaterial
-            wireframe={true}
-          />
-        </mesh>
+      <Canvas
+        style={{ height: "100vh", width: "100vw" }}
+        camera={{ fov: 45, position: [15, 4, 0] }}>
+        <ambientLight color="0x404040" intensity={0.8} />
+        <Suspense>
+          {/* Cylinder */}
+          <Cylinder position={[0, 0, 0]}/>
+          <Cylinder position={[0, 0, 4]}/>
+          <Cylinder position={[0, 0, -4]}/>
+          {/* Box */}
+          {/* <Box position={[0, 0, 0]}/>
+          <Box position={[0, 0, 4]}/>
+          <Box position={[0, 0, -4]}/> */}
+        </Suspense>
+        <OrbitControls />
       </Canvas>
     </div>
   );
